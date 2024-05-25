a652a4984987282a8115ec6e0c15c60587a573a2
ZurabDz <dzindzibadzezurabi@gmail.com>
2024-05-25 11:24:04 -0700

    running pre process script

---
+++ run_preprocess.sh
@@ -0,0 +1,8 @@
+python preprocess.py \
+    --dataset_name uonlp/CulturaX \
+    --dataset_config_name ka \
+    --validation_split_percentage 1 \
+    --max_seq_length 32 \
+    --preprocessing_num_workers 14 \
+    --tokenizer_name ./tok2/ \
+    --token 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
