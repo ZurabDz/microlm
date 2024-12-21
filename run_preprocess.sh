da1482a53de7ea98f16a3696229ad5ab65482d3b
ZurabDz <dzindzibadzezurabi@gmail.com>
2024-12-21 20:22:19 +0400

    deleting old hf code

--- run_preprocess.sh
+++
@@ -1,8 +0,0 @@
-python preprocess.py \
-    --dataset_name uonlp/CulturaX \
-    --dataset_config_name ka \
-    --validation_split_percentage 1 \
-    --max_seq_length 32 \
-    --preprocessing_num_workers 14 \
-    --tokenizer_name ./tok2_30_000/ \
-    --token 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
