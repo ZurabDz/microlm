213985979edeac4b19a160b1e15f6caf184e74a9
ZurabDz <dzindzibadzezurabi@gmail.com>
2025-02-08 16:02:45 +0400

    simple bpe tokenizer training

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,59 +1,26 @@
-import zstandard as zstd
-from tokenizers import SentencePieceBPETokenizer
-import json
 from datasets import load_dataset
-from tqdm import tqdm
+from tokenizers import Tokenizer, decoders, pre_tokenizers
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.normalizers import NFKC
 
 
-# Loading tiny stories dataset
-ds = load_dataset("roneneldan/TinyStories")
+ds = load_dataset('DKYoon/SlimPajama-6B', num_proc=6)
+tokenizer = Tokenizer(BPE(unk_token='<unk>'))  # do I need unk
 
 
-# # Writing train data to a compressed JSONL file
-# with zstd.open("tiny_stories_train.jsonl.zst", "w") as f:
-#     for record in tqdm(ds['train']):
-#         f.write(json.dumps(record) + "\n")
+tokenizer.normalizer = NFKC()
+tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=False)
+tokenizer.decoder = decoders.ByteLevel()
 
+data = ds['test']
 
-def iterate_over_zst_data(path):
-    with zstd.open(path, "r") as f:
-        for line in f:
-            record = json.loads(line)['text']
-            yield record
-
-# def iterate_over_zst_data(path):
-#     dctx = zstd.ZstdDecompressor()
-#     with open(path, 'rb') as compressed:
-#         reader = dctx.stream_reader(compressed)
-#         buffer = ''
-#         while True:
-#             chunk = reader.read(2**20)  # 1MB chunks
-#             if not chunk:
-#                 break
-#             buffer += chunk.decode('utf-8')
-#             lines = buffer.split('\n')
-
-#             data = []
-#             for line in lines[:-1]:  # Process all lines except the last one which might be incomplete
-#                 if line:  # Skip empty lines if any
-#                     try:
-#                         record = json.loads(line)
-#                         data.append(record.get('text', None))
-#                     except json.JSONDecodeError:
-#                         print(f"Error decoding line: {line}")
-
-#             yield data
-#             buffer = lines[-1]  # Keep the last (possibly incomplete) line for the next iteration
-
-tokenizer = SentencePieceBPETokenizer()
-train_data_iterator = iterate_over_zst_data('tiny_stories_train.jsonl.zst')
+trainer = BpeTrainer(vocab_size=30_000, min_frequency=2, show_progress=True,
+                     special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>"],
+                     initial_alphabet=pre_tokenizers.ByteLevel.alphabet())
 
 tokenizer.train_from_iterator(
-    train_data_iterator,
-    vocab_size=30000,
-    min_frequency=2,
-    show_progress=True,
-    special_tokens=["<|begin_of_text|>", "<|end_of_text|>"],
+    (entry['text'] for entry in data.iter(batch_size=1000)), trainer, length=len(data)
 )
 
-tokenizer.save_model('./')
+tokenizer.save('fresh_tokenizer_bpe.json')
